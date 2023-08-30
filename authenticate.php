ff5c9c6d0d8ca8da9a5187ac018c150788622a1b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2023-08-30 16:07:02 -0300

    verifyCode

--- authenticate.php
+++ authenticate.php
@@ -22,7 +22,6 @@ $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
-<<<<<<< HEAD
 function updateTypeToZero($conn, $user_id)
 {
     $typeUpdate = "UPDATE users SET type = '0' WHERE id = ?";
@@ -30,8 +29,6 @@ function updateTypeToZero($conn, $user_id)
     $stmt->bind_param("i", $user_id);
     $stmt->execute();
 }
-=======
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
 
 if ($result && $result->num_rows > 0) {
     $row = $result->fetch_assoc();
@@ -43,38 +40,33 @@ if ($result && $result->num_rows > 0) {
         $nomeDaPessoa = $user_name;
         $aviso = "Escaneie o QR Code abaixo com o aplicativo Google Authenticator:";
         $qrCodeUrl = $ga->getQRCodeGoogleUrl("Plugin Logix Web: $nomeDaPessoa", $user_secret);
-<<<<<<< HEAD
-=======
 
         $typeUpdate = "UPDATE users SET type = '0' WHERE id = $user_id";
         $conn->query($typeUpdate);
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $code = $_POST["code"];
-<<<<<<< HEAD
         if ($ga->verifyCode($user_secret, $code)) {
             $_SESSION["google_authenticated"] = true;
             header("Location: ./pages/home.php");
             if (isset($_POST["updateButton"])) {
                 updateTypeToZero($conn, $user_id);
             }
-=======
 
-        if ($ga->verifyCode($user_secret, $code)) {
-            $_SESSION["google_authenticated"] = true;
-            header("Location: ./pages/home.php");
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
-            exit();
-        } else {
-            echo '<script>alert("Código inválido. Por favor, tente novamente.");</script>';
-            header("Location: login.php");
-            die();
+            if ($ga->verifyCode($user_secret, $code)) {
+                $_SESSION["google_authenticated"] = true;
+                header("Location: ./pages/home.php");
+                exit();
+            } else {
+                echo '<script>alert("Código inválido. Por favor, tente novamente.");</script>';
+                header("Location: login.php");
+                die();
+            }
         }
+    } else {
+        $error_message = "Erro ao buscar o segredo do usuário.";
     }
-} else {
-    $error_message = "Erro ao buscar o segredo do usuário.";
 }
 
 $stmt->close();
@@ -92,7 +84,6 @@ $conn->close();
 <body>
     <div class="container-form-authenticate">
         <div class="container-form-qr">
-<<<<<<< HEAD
             <p class=" qrcode-unavailable">
                 <?php echo "$aviso"; ?>
                 <img src="<?php
@@ -100,49 +91,39 @@ $conn->close();
                     echo "$qrCodeUrl";
                 }
                 ?>">
-=======
-            <p class=" qrcode-unavailable"><?php echo "$aviso"; ?>
+
+            <p class=" qrcode-unavailable">
+                <?php echo "$aviso"; ?>
                 <img src="<?php
-                            if ($user_type == 1) {
-                                echo "$qrCodeUrl";
-                            }
-                            ?>">
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
+                if ($user_type == 1) {
+                    echo "$qrCodeUrl";
+                }
+                ?>">
             </p>
         </div>
     </div>
     <div class="container-form">
         <h2>Autenticação Plugin Web:</h2>
         <form method="post" action="">
-<<<<<<< HEAD
             <h3>Olá
                 <?= $user_name; ?>, seja bem-vindo(a)!
             </h3>
             <label for="code">Código de Autenticação:</label>
             <input type="text" id="code" name="code" required><br><br>
             <button type="submit" name="updateButton">Verificar</button>
-=======
-            <h3>Olá <?= $user_name; ?>, seja bem-vindo(a)!</h3>
+
+            <h3>Olá
+                <?= $user_name; ?>, seja bem-vindo(a)!
+            </h3>
             <label for="code">Código de Autenticação:</label>
             <input type="text" id="code" name="code" required><br><br>
             <button type="submit">Verificar</button>
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
         </form>
 
         <?php if (isset($error_message)) {
             echo "<p>$error_message</p>";
         } ?>
     </div>
-
-    <script>
-<<<<<<< HEAD
-        document.addEventListener("DOMContentLoaded", function () {
-=======
-        document.addEventListener("DOMContentLoaded", function() {
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
-            document.querySelector(".qrcode-container").classList.add("animate-slide");
-        });
-    </script>
 </body>
 
 </html>
--- config/authenticator_script_db.sql
+++ config/authenticator_script_db.sql
@@ -1,9 +1,7 @@
 # SELECT:
-<<<<<<< HEAD
 SELECT * FROM users;
-=======
 select * from users;
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
+
 
 # INSERT:
 INSERT INTO users (name, password)
@@ -14,11 +12,9 @@ VALUES ("user01", "@123"),
        ("user05", "@123");
 
 INSERT INTO users (name, password)
-<<<<<<< HEAD
 VALUES ("Marco", "@123");
-=======
 VALUES ("Henrique Maximo", "@123");
->>>>>>> 49b805e47a591624e7f94d6df867af5590dc3caa
+
 
 # UPDATE:
 UPDATE users SET type = 1 WHERE id = 6;
